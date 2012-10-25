f6f193ad9181377f095d7332057d0c82dd687433
Alex Jokela <ajokela@umn.edu>
2012-10-25 15:18:14 -0500

    Making a new GEM

---
+++ lib/acolyte.rb
@@ -0,0 +1,97 @@
+module RABL
+  
+  module Database  
+    module Transaction
+      
+      def self.block(enable, &b)
+      
+        if enable
+          ActiveRecord::Base.transaction do
+            b.call
+          end
+        else
+          b.call
+        end
+        
+      end
+      
+    end
+  end
+  
+  module Utilities
+    
+    def self.spin
+      iter = true
+      
+      thread = Thread.new do
+        while iter do
+          sleep 1
+        end
+      end
+      
+      yield.tap {
+        iter = false
+        thread.join
+      }
+      
+    end
+    
+    def self.wait_spinner( args = {} )
+    
+      defaults = {:fps=> 10, :enable => true, :debug => 0}
+      
+      if args.class == Hash
+        args = defaults.merge(args)
+      else
+        args = defaults
+      end
+      
+      unless args[:debug].nil?
+        if args[:debug].to_s.to_i > 3
+          args[:enable] = false
+        end
+      end
+      
+      unless ENV['DEBUG'].nil?
+        if ENV['DEBUG'].to_i > 3
+          args[:enable] = false
+        end
+      end
+      
+      unless ENV['DISABLE_SPINNER'].nil?
+        args[:enable] = false
+      end
+      
+      unless ENV["RAILS_ENV"].nil?
+        if ENV["RAILS_ENV"].match(/test/)
+          args[:enable] = false
+        end
+      end
+      
+      chars = %w[| / - \\]
+      delay = 1.0/args[:fps].to_f
+      iter = 0
+      
+      if args[:enable]
+      
+        spinner = Thread.new do
+          while iter do  # Keep spinning until told otherwise
+            $stderr.print chars[(iter+=1) % chars.length]
+            sleep delay
+            $stderr.print "\b"
+          end
+        end
+    
+        yield.tap{       # After yielding to the block, save the return value
+          iter = false   # Tell the thread to exit, cleaning up after itself…
+          spinner.join   # …and wait for it to do so.
+        }                # Use the block's return value as the method's
+      
+      else
+        yield
+      end
+      
+    end  
+  end
+  
+end
---
+++ rabl.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name    = "rabl"
+  s.version     = '0.1'
+  s.date        = '2012-10-25'
+  s.summary     = "YML Data + MetaProgrammery = RABL"
+  s.description = "Data and the sugar for describing relationships between these data"
+  s.authors     = ["Alex Jokela", "Peter Clark"]
+  s.email       = 'ajokela@umn.edu'
+  s.files       = ['lib/rabl.rb']
+  s.homepage    = 'http://users.pop.umn.edu/~ajokela/#RABL'
+end
